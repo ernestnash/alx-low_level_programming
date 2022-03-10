659bf5b611f00204534d5312faf98cc485f0d99f
ernestnash <eernestnnashon@gmail.com>
2022-03-10 19:19:28 +0300

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of all C code
+ *
+ * 
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
